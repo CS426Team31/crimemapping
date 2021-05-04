add82e035639fed4783f486284e7b5c8ee5503ff
SteveH97 <78455352+SteveH97@users.noreply.github.com>
2021-05-04 13:05:41 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,19 @@
-import firebase from "firebase";
-import "firebase/database";
+import firebase from 'firebase/app';
+import 'firebase/database';
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN_KEY,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-});
+//This is the key given from the firebase console which helps the program connect to 
+//the database
+const firebaseConfig = {
+  apiKey: "AIzaSyCGzcG7VkEY2sDKbvCDrUdmIYrya0Z9GgQ",
+  authDomain: "seniorproj3-bb891.firebaseapp.com",
+  databaseURL: "https://seniorproj3-bb891-default-rtdb.firebaseio.com",
+  projectId: "seniorproj3-bb891",
+  storageBucket: "seniorproj3-bb891.appspot.com",
+  messagingSenderId: "269338562487",
+  appId: "1:269338562487:web:9912c7b5542ace3b4c2232",
+  measurementId: "G-55SCE1Z8JN"
+  };
 
-const db = firebaseApp.database();
-export default db;
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.database();
